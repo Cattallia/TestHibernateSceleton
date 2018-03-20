ed6e821681c631a6225cc93680c379fc576e0680
Joseph Stalin <stalin@comunism.com>
2018-03-20 19:20:11 +0200

    ...

---
+++ TestHibernateSceleton/my-project/org/itstep/App.java
@@ -0,0 +1,18 @@
+package org.itstep;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.itstep.model.Account;
+import org.itstep.util.HibernateUtil;
+
+/**
+ * @author Alex Ignatenko
+ */
+public class App {
+	public static void main(String[] args) {
+//		Account account = new Account("ignatenko22071", "1234561", "Alex1", "Ignatenko1", new Date(82, 7, 22).getTime());
+
+//		System.out.println("Transactions completed!");
+	}
+}
---
+++ TestHibernateSceleton/my-project/org/itstep/util/HibernateUtil.java
@@ -0,0 +1,37 @@
+package org.itstep.util;
+
+import java.io.File;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory = configureSessionFactory();
+
+	 /**
+	  * Create SessionFactory
+	  * 
+	  * @return {@link SessionFactory}
+	  * @throws HibernateException
+	  */
+	 private static SessionFactory configureSessionFactory() throws HibernateException {
+
+	  String userDir = System.getProperty("user.dir");
+
+	  Configuration configuration = new Configuration().configure(new File(userDir + "/src/main/resources/hibernate.cfg.xml"));
+	  
+	  return configuration.buildSessionFactory();
+
+	 }
+
+	 /**
+	  * Get SessionFactory
+	  * 
+	  * @return {@link SessionFactory}
+	  */
+	 public static SessionFactory getSessionFactory() {
+	  return sessionFactory;
+	 }
+}
